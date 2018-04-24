a8423a6aad3c0994905070d750bee23613ef56b4
hcote <hcote12@gmail.com>
2018-04-24 14:27:19 -0700

    added footer. added logo. added jQuery to color-code percent changes (green or red)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,41 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+var oneDayChange = parseFloat($('.1d').html());
+var sevenDayChange = parseFloat($('.7d').html());
+var zero = 0;
+console.log(oneDayChange);
+console.log(sevenDayChange);
+
+
+
+$(".1d").each(function(i) {
+  if (parseFloat($(this).html()) > 0) {
+    $(this).removeClass('red');
+    $(this).addClass('green');
+  } else {
+    $(this).removeClass('green');
+    $(this).addClass('red');
+    }
+});
+
+
+$(".7d").each(function(i) {
+  if (parseFloat($(this).html()) > 0) {
+    $(this).removeClass('red');
+    $(this).addClass('green');
+  } else {
+    $(this).removeClass('green');
+    $(this).addClass('red');
+    }
+});
+
+
 $('.oneday').on('click', function() {
   $('.7d').addClass('hide');
   $('.1d').removeClass('hide');
+  $('.7day').addClass('hide');
+  $('.1day').removeClass('hide');
   $('.oneday').addClass('gray');
   $('.sevenday').removeClass('gray');
 })
@@ -11,8 +43,12 @@ $('.oneday').on('click', function() {
 $('.sevenday').on('click', function() {
   $('.1d').addClass('hide');
   $('.7d').removeClass('hide');
+  $('.1day').addClass('hide');
+  $('.7day').removeClass('hide');
   $('.sevenday').addClass('gray');
   $('.oneday').removeClass('gray');
 })
+
+
 
 });
